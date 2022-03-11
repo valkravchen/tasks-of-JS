dafd2349b49e6c6efe9390a1b388ffb2b5ea861d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-03-11 21:53:44 +0300

    add new file

---
+++ 13.js
@@ -0,0 +1,9 @@
+let eurosCount = 100;
+
+let dollarsPerEuro = 1.25;
+let rublesPerDollar = 60;
+
+let dollarsCount = eurosCount * dollarsPerEuro;
+console.log(dollarsCount);
+let rublesCount = dollarsCount * rublesPerDollar;
+console.log(rublesCount);
