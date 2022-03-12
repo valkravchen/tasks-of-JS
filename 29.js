73eaec2d284ace2f6d3fee86f04fc798c0e82158
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-03-12 17:50:57 +0300

    add new files

---
+++ 29.js
@@ -0,0 +1,4 @@
+const text = 'Never forget what you are, for surely the world will not';
+
+
+console.log(`First: ${text[0]}\nLast: ${text[text.length - 1]}`);
---
+++ 30.js
@@ -0,0 +1,3 @@
+const text = 'When \t\n you play a \t\n game of thrones you win or you die.';
+
+console.log(text.substring(4, 15).trim().length);
