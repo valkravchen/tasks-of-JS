28643955cef07dc6183dd8279eb0162a4cfe9afe
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-04-17 22:27:10 +0300

    Delete 14.js

--- 14.js
+++
@@ -1,8 +0,0 @@
-let info = "We couldn't verify you mother's maiden name.";
-let intro = 'Here is important information about your account security.';
-
-let firstName = 'Joffrey';
-let greeting = 'Hello';
-
-console.log(greeting + ', ' + firstName + '!');
-console.log(intro + '\n' + info);
