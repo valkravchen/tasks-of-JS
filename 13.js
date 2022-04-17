d6df13970586dedb39e5a3ffd67737764b891f71
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-04-17 22:27:02 +0300

    Delete 13.js

--- 13.js
+++
@@ -1,9 +0,0 @@
-let eurosCount = 100;
-
-let dollarsPerEuro = 1.25;
-let rublesPerDollar = 60;
-
-let dollarsCount = eurosCount * dollarsPerEuro;
-console.log(dollarsCount);
-let rublesCount = dollarsCount * rublesPerDollar;
-console.log(rublesCount);
