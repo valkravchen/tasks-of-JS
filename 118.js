bbf2e09922cbd6ece46949d8691b8b01a5e9d955
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-04-17 22:26:43 +0300

    Delete 118.js

--- 118.js
+++
@@ -1,9 +0,0 @@
-function eachCons(array, n) {
-    let res = [];
-
-    for(let i = 0; i < array.length; i++){
-        res.push(array.slice(i,i + n));
-    }
-
-    return res.filter(e => e.length === n);
-}
