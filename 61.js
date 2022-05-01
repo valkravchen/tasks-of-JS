1b68cde31c8447de278339356119c632691d6284
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-05-01 22:59:18 +0300

    Delete 61.js

--- 61.js
+++
@@ -1,5 +0,0 @@
-const name1 = 'Robb';
-const name2 = 'Shaya';
-console.log(name2.length); // => 5
-console.log(name2.toLowerCase()); // => shaya
-console.log(Math.min(name1.length, name2.length)); // => 4
