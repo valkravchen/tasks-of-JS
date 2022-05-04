caa8f1ae65273f28fce3fe0517c8cf840d71de8e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-05-04 20:59:22 +0300

    Delete 69.js

--- 69.js
+++
@@ -1,7 +0,0 @@
-const hasSpecialChars = (str) => /* проверяет содержание специальных символов в строке */;
-
-const isStrongPassword = (password) => {
-    const length = password.length;
-    // Скобки задают приоритет. Понятно что к чему относится.
-    return (length > 8 && length < 20) || hasSpecialChars(password);
-};
