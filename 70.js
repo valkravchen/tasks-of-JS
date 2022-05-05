7dd772e111eba11f4451893cf5e5ddcb1ab2da50
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-05-05 21:48:13 +0300

    Delete 70.js

--- 70.js
+++
@@ -1,5 +0,0 @@
-const isGoodApartment = (area, street) => {
-    // Через переменную, чтобы функция была не слишком длинной
-    const result = area >= 100 || (area >= 80 && street === 'Main Street');
-    return result;
-};
