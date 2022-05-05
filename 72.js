a5042b54bb14013c793ff931f04b95384c5fed99
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-05-05 21:48:28 +0300

    Delete 72.js

--- 72.js
+++
@@ -1,6 +0,0 @@
-const isPalindrome = (word) => {
-    const lowerWord = word.toLowerCase();
-    return lowerWord === reverse(lowerWord);
-};
-
-const isNotPalindrome = (word) => !isPalindrome(word);
